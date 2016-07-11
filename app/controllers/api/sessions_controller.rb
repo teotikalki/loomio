f486ffd3fc8fcb00779ef1acbe7dc65ecea065ec
James Kiesel <james@loomio.org>
2016-07-11 00:36:17 -0400

    Tweak overwritten sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,17 +1,17 @@
 class API::SessionsController < Devise::SessionsController
-  include DeviseControllerHelper
 
   def create
-    if user = warden.authenticate(scope: resource_name)
-      sign_in resource_name, user
-      render json: CurrentUserSerializer.new(user).as_json
+    if self.resource = warden.authenticate(scope: resource_name)
+      sign_in(resource_name, resource)
+      flash[:notice] = t(:'devise.sessions.signed_in')
+      render json: CurrentUserSerializer.new(resource).as_json
     else
       render json: { errors: { password: [t(:"devise.failure.invalid")] } }, status: 401
     end
   end
 
   def destroy
-    sign_out resource_name
+    sign_out
     flash[:notice] = t(:'devise.sessions.signed_out')
     head :ok
   end
